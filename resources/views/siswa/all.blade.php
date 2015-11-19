acdc8d63bd4ae24d6d7790d8ffb64cd2d24d5112
Arief Setya <email@ariefsetya.com>
2015-11-19 09:01:23 +0700

    commit pertama

---
+++ resources/views/siswa/all.blade.php
@@ -0,0 +1,46 @@
+@extends('app')
+@section('content')
+<div class="container">
+	<div class="row">
+		<div class="col-md-10 col-md-offset-1">
+			<div class="panel panel-default">
+				<div class="panel-heading">All Siswa</div>
+
+				<div class="panel-body">
+	<span style="float:right;">
+		<a class="btn btn-primary" 
+		href="{{ url('siswa/add') }}">Add New</a>
+	</span>
+	<table class="table">
+		<thead>
+			<tr>
+				<th>Nama</th>
+				<th>Kelas</th>
+				<th>Jurusan</th>
+				<th>Tempat Lahir</th>
+				<th>Tanggal Lahir</th>
+				<th>Action</th>
+			</tr>
+		</thead>
+		<tbody>
+			@foreach($data as $key)
+			<tr>
+				<td>{{$key->nama}}</td>
+				<td>{{$key->kelas}}</td>
+				<td>{{$key->jurusan}}</td>
+				<td>{{$key->tempat_lahir}}</td>
+				<td>{{$key->tanggal_lahir}}</td>
+				<td>
+					<a href="{{url('siswa/edit/'.$key->id)}}">Edit</a>
+					<a onclick="return confirm('Hapus?')" 
+					href="{{url('siswa/delete/'.$key->id)}}">Delete</a>
+				</td>
+			</tr>
+			@endforeach
+		</tbody>
+	</table></div>
+			</div>
+		</div>
+	</div>
+</div>
+@endsection
